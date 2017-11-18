4dcf0fe4aa5747c8b0634b8bacb06da4b7076e61
scribblink <james.woods396@gmail.com>
2017-11-18 11:45:49 -0500

    update dependencies

---
+++ components/RichMedia.js
@@ -0,0 +1,12 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import { Html } from '../html';
+
+export default function RichMedia({ body }) {
+  console.warn('RichMedia is deprecated and will be removed in next versions.');
+  return <Html body={body} />;
+}
+
+RichMedia.propTypes = {
+  body: PropTypes.string.isRequired,
+};
